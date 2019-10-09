26bfe7c76046b8384379b1df7c5427746a4b84e6
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-09 20:02:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+class Dog 
+ 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.clear_all
+    
+   # @@all.each {|ele|}.delete(ele)
+   @@all.clear
+  end 
+  
+  def self.print_all
+    output = ""
+    @@all.each {|ele| output += ele}.to_str
+    puts output
+  end 
+  
+  def save 
+    @@all 
+    
+  end
+  
+
+  
+  
+end 
